82eb0f4f6778b8ce10efc461ba20e105f390d648
CyganFx <duman070601@gmail.com>
2020-11-15 23:28:59 +0600

    cookie destroy in logout

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -2,10 +2,7 @@ package servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet(name = "LogoutServlet")
@@ -18,6 +15,11 @@ public class LogoutServlet extends HttpServlet {
         HttpSession httpSession = request.getSession();
         httpSession.removeAttribute("username");
         httpSession.invalidate();
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
         response.sendRedirect("index.jsp");
     }
 }
